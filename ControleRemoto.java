88003c1eb73ce017169c99469343a8b72227a169
Sidney Bispo <83840984+sidneybispo@users.noreply.github.com>
2021-07-16 09:22:28 -0300

    Add files via upload

---
+++ ControleRemoto.java
@@ -0,0 +1,15 @@
+package controleremoto;
+//Autor: Sidney Bispo
+public class ControleRemoto {
+    public static void main(String[] args) {
+        ArqControle c = new ArqControle();
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+        
+        
+    }
+    
+}
